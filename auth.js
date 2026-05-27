d3fb7d21a763c97412b09d8f01f83e0e25d64b2c
jesusxal777-boop <jesusxal777@gmail.com>
2026-05-27 09:56:06 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
-// auth.js
 const db = supabase.createClient(
-    'https://tu-url-de-supabase.supabase.co', 
-    'tu-anon-key'
+    'https://qynkgbgxavjgmvnskbsd.supabase.co', 
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5bmtnYmd4YXZqZ212bnNrYnNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDQ5NzMsImV4cCI6MjA5NTQyMDk3M30.2lbtOLD-pgKYYrJiNsaOIsw3oM3jaZUgccDA-68Y6JI'
 );
+
 
 // Función reutilizable para checar sesión
 async function checkAuth(redirectIfNotAuth = true) {
