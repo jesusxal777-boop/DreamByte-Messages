716fc3510dfde258ab93d5e9d52af4871520800f
jesusxal777-boop <jesusxal777@gmail.com>
2026-05-27 10:04:25 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,10 +3,14 @@ const db = supabase.createClient(
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5bmtnYmd4YXZqZ212bnNrYnNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDQ5NzMsImV4cCI6MjA5NTQyMDk3M30.2lbtOLD-pgKYYrJiNsaOIsw3oM3jaZUgccDA-68Y6JI'
 );
 
+async function loginUser(email, password) {
+    const { data, error } = await db.auth.signInWithPassword({ email, password });
+    if (error) throw error;
+    return data;
+}
 
-// Función reutilizable para checar sesión
-async function checkAuth(redirectIfNotAuth = true) {
-    const { data: { session } } = await db.auth.getSession();
-    if (!session && redirectIfNotAuth) location.href = 'index.html';
-    return session;
+async function registerUser(email, password) {
+    const { data, error } = await db.auth.signUp({ email, password });
+    if (error) throw error;
+    return data;
 }
